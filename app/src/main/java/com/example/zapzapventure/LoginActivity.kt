53cbfa6407966277d62db3b01d4534185b54da25
Gabriela Casagrande <gabriela_casagrande86@hotmail.com>
2021-07-03 11:32:04 -0300

    Layouts

--- app/src/main/java/com/example/zapzapventure/LoginActivity.kt
+++ app/src/main/java/com/example/zapzapventure/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.zapzapventure
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -22,6 +23,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(Color.WHITE)
 
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build() ,
